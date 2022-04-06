b83abec87b89b831f0a3eb553fd793e7e8414580
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-04-06 13:54:00 +1000

    Update products

--- lib/products.ts
+++ lib/products.ts
@@ -1,17 +1,33 @@
 export const products = [
   {
-    id: 'box-of-cookies',
-    name: 'Box',
-    description: 'A delicious box of handmade cookies',
-    unitName: 'box', // shows after the price, eg. 0.05 SOL/box
+    id: 'bacon',
+    name: 'Bacon',
+    description: 'A delicious strip of bacon',
+    unitName: 'strip', // shows after the price, eg. 0.05 SOL/box
     priceSol: 0.05,
     priceUsd: 5,
   },
   {
-    id: 'basket-of-cookies',
-    name: 'Basket',
-    description: 'A large basket of handmade cookies',
-    unitName: 'basket',
+    id: 'steak',
+    name: 'Steak',
+    description: 'A large hunk of steak',
+    unitName: 'hunk',
+    priceSol: 0.1,
+    priceUsd: 10,
+  },
+  {
+    id: 'chicken',
+    name: 'Chicken',
+    description: 'A large hunk of chicken',
+    unitName: 'hunk',
+    priceSol: 0.1,
+    priceUsd: 10,
+  },
+  {
+    id: 'pork',
+    name: 'Pork',
+    description: 'A large hunk of pork',
+    unitName: 'hunk',
     priceSol: 0.1,
     priceUsd: 10,
   }
