1a3f217b2e35eb6d47a835767a7ed71ee5d55eec
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-04-07 14:36:12 +1000

    Add Shop Address

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -1,4 +1,4 @@
 import { PublicKey } from "@solana/web3.js"
 
 // Your shop wallet address
-export const shopAddress = new PublicKey('...') 
+export const shopAddress = new PublicKey('9b61g2yCd3ixiDYjYdyz54n37aSq9Jnz6Hg69YXq81nr') 
