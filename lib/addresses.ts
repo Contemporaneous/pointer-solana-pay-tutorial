e4a81352d4270f4e16bed8ed05efe3b6b014ee76
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-04-17 13:36:28 +1000

    Update makeTransaction api

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -2,3 +2,4 @@ import { PublicKey } from "@solana/web3.js"
 
 // Your shop wallet address
 export const shopAddress = new PublicKey('9b61g2yCd3ixiDYjYdyz54n37aSq9Jnz6Hg69YXq81nr') 
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
