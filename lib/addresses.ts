eda826fa21975aa1bd00826b0cf81876acea84fe
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-04-17 14:32:14 +1000

    Add CouponBook component

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -3,3 +3,4 @@ import { PublicKey } from "@solana/web3.js"
 // Your shop wallet address
 export const shopAddress = new PublicKey('9b61g2yCd3ixiDYjYdyz54n37aSq9Jnz6Hg69YXq81nr') 
 export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+export const couponAddress = new PublicKey('HPfTo8tP6XADSTnMAMtFLqPxHY68DoMhvTPJbfnFdvwo')
